ac0c2f5730b08d32259a001f1788ee6f09d20c66
Atharv Maan <atharv24@live.in>
2018-12-10 19:00:56 +0530

    add incomplete network class and game manager

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Network
+{
+    private List<Genome.NodeGene> nodes;
+    private Dictionary<int, Genome.ConnectionGene> connections;
+    private float fitness;
+	
+    public Network(Genome genome)
+    {
+        nodes = genome.GetNodes();
+        connections = genome.GetConnections();
+
+    }
+
+    public float GetFitness()
+    {
+        return fitness;
+    }
+
+    public void SetFitness(float fit)
+    {
+        fitness = fit;
+    }
+
+    public void AddFitness(float fit)
+    {
+        fitness += fit;
+    }
+
+}
